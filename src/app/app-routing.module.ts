50604dcd12a81980b9856819da7f9b512f21c952
Bao Phu Tuong <phutuongbao1999@gmail.com>
2023-04-03 18:31:55 +0700

    Setting Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./modules/welcome/welcome.module').then((m) => m.WelcomeModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
