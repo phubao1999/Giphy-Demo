54994921b01e27f75f3326d4d07b249c988eb982
Bao Phu Tuong <phutuongbao1999@gmail.com>
2023-04-06 02:00:52 +0700

    Pre-Render Gif

--- src/app/shared/components/gif-card/gif-card.component.ts
+++ src/app/shared/components/gif-card/gif-card.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input, TemplateRef } from '@angular/core';
-import { IGif } from '../../model';
+import {
+  Component,
+  Input,
+  OnInit,
+  TemplateRef
+} from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { IGif } from '../../model';
 
 @Component({
   selector: 'app-gif-card',
   templateUrl: './gif-card.component.html',
   styleUrls: ['./gif-card.component.scss'],
 })
-export class GifCardComponent {
+export class GifCardComponent implements OnInit {
   @Input() data!: IGif;
   isVisible = false;
   isLoading = false;
+  randomColor!: string;
 
-  constructor(private notify: NzNotificationService) {}
+  constructor(
+    private notify: NzNotificationService,
+  ) {}
+  ngOnInit(): void {
+    this.randomColor = this.randomRgba();
+  }
 
   showModal(): void {
     this.isVisible = true;
@@ -26,4 +37,22 @@ export class GifCardComponent {
     this.notify.template(template);
     navigator.clipboard.writeText(url);
   }
+
+  randomRgba() {
+    let o = Math.round,
+      r = Math.random,
+      s = 255;
+    return (
+      'rgba(' +
+      o(r() * s) +
+      ',' +
+      o(r() * s) +
+      ',' +
+      o(r() * s) +
+      ',' +
+      r().toFixed(1) +
+      ')'
+    );
+  }
 }
+// [ngStyle]="{background: rgb(255, 243, 92)}"
