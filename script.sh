9825214e5979a9a3cc34cd9ab5686d92626f2148
Bao Phu Tuong <phutuongbao1999@gmail.com>
2023-07-02 03:18:56 +0700

    Script EX FE

--- script.sh
+++ script.sh
@@ -8,11 +8,12 @@ echo "Git Step Processing ...."
 git checkout $branch
 git pull origin
 
-echo "Remove Old Folder"
-rm -r ./build
+echo "Build Step"
+npm install --legacy-peer-deps
+npm run build
 
-echo "Building And Deploy Step"
-# echo "Docker Build"
-# docker build -t fe-image .
-# echo "Docker Container"
-# docker run -d -p 80:80 --name fe-container fe-image
+echo "Replace old build folder to new folder"
+cp -rf ./dist /var/www/html
+
+echo "Restart Service For Nginx"
+sudo service nginx restart
